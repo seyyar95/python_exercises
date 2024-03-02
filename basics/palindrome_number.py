1a7f8d6e142ab1da6b11e55b82787f87090c0694
seyyar95 <heydrovsyyar@gmail.com>
2024-03-02 10:53:29 +0000

    palindrome number

---
+++ basics/palindrome_number.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+
+list_number = input("input number: ")
+str_number = str(list_number)
+if list_number[0] == list_number[-1]:
+    print(f"Yes the number {str_number} is palindrome number")
+    """
+
+
+
+def palindrome(number):
+    print("original number", number)
+    original_num = number
+    
+    # reverse the given number
+    reverse_num = 0
+    while number > 0:
+        reminder = number % 10
+        reverse_num = (reverse_num * 10) + reminder
+        number = number // 10
+        
+    # check numbers
+    if original_num == reverse_num:
+        print("Given number palindrome")
+    else:
+        print("Given number is not palindrome")
+
+palindrome(121)
+palindrome(125)
