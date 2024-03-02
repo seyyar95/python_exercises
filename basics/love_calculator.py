ce2e9895b1f23dded5b309d904888167a9a1d329
seyyar95 <heydrovsyyar@gmail.com>
2024-03-02 07:16:59 +0000

    Code Modified

--- basics/love_calculator.py
+++ basics/love_calculator.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
+def love_calculator(name1, name2):
+    """
+    Calculates the love score between two names.
+
+    Args:
+        name1: the first name.
+        name2: The second name.
+    Returns:
+        A string representing the love score and a message.
+    """
+    combined_string = name1.lower() + name2.lower()
+
+    true_count = combined_string.count('t') + combined_string.count('r') + combined_string.count('u') + combined_string.count('e')
+    love_count = combined_string.count('l') + combined_string.count('o') + combined_string.count('v') + combined_string.count('e')
+
+    score = int(str(true_count) + str(love_count))
+
+    if score <= 10 or score >= 90:
+        return f"Your score is {score} and you go together like coke and mentos"
+    elif score >= 40 and score <= 50:
+        return f"Your score is {score} and you are alright together"
+    else:
+        return f"Your score is {score}"
+
+
+
+
 name1 = input("Enter your name: ")
 name2 = input("Enter his/her name: ")
-combined_string = name1 + name2
-lower_case_string = combined_string.lower()
-
-t = lower_case_string.count('t')
-r = lower_case_string.count('r')
-u = lower_case_string.count('u')
-e = lower_case_string.count('e')
-
-true = t + r + u + e
-
-l = lower_case_string.count('l')
-o = lower_case_string.count('o')
-v = lower_case_string.count('v')
-e = lower_case_string.count('e')
-
-love = l + o + v + e
-score = int(str(true) + str(love))
-
-if score < 10 or score > 90:
-    print(f"Your score is {score} and you go together like coke and mentos")
-elif score >= 40 and score <= 50:
-    print(f"Your score is {score} and you are alright together")
-else:
-    print(f"Your score is {score}")
+print(love_calculator(name1, name2))
