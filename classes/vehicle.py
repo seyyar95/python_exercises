dd3735b470020d389983802aa2dabb947817b9d1
seyyar95 <heydrovsyyar@gmail.com>
2024-03-13 12:39:49 +0000

    Vehicle class is updated

--- classes/vehicle.py
+++ classes/vehicle.py
@@ -2,10 +2,13 @@
 
 
 class Vehicle:
-    def __init__(self, model, max_speed, mileage):
+    def __init__(self, name, max_speed, mileage):
         self.max_speed = max_speed
         self.mileage = mileage
-        self.model = model
+        self.name = name
 
     def __str__(self):
-        return f"Model: {self.model}\nMax Speed: {self.max_speed}\nMileage: {self.mileage}"
+        return f"Model: {self.name}\nMax Speed: {self.max_speed}\nMileage: {self.mileage}"
+
+    def seating_capacity(self, capacity):
+        return f"The seating capacity of a {self.name} is {capacity} passengers"
