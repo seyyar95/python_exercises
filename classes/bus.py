0e2663a8257e8191a12e6d6a6ba01008f0e15b73
seyyar95 <heydrovsyyar@gmail.com>
2024-03-13 12:39:27 +0000

    BUs is ready to go

--- classes/bus.py
+++ classes/bus.py
@@ -2,9 +2,11 @@
 from vehicle import Vehicle
 
 class Bus(Vehicle):
-    pass
+    def seating_capacity(self, capacity=50):
+        return super().seating_capacity(capacity)
 
 
 if __name__ == "__main__":
     bus = Bus("School Bus", "180", "12")
     print(bus)
+    print(bus.seating_capacity())
